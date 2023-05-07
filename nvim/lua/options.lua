bbf5e13c6b31e6392e5986477a2497e845613bbe
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-06 19:28:55 -0700

    options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,7 +1,11 @@
 -- Editor options
 vim.opt.number = true
 vim.opt.wrap = true
+vim.opt.breakindent = true
 vim.opt.scrolloff = 5
+vim.opt.mouse = 'a'
+vim.opt.signcolumn = 'yes'
+vim.opt.showcmd = false
 
 -- Tab options
 vim.opt.expandtab = true
@@ -14,6 +18,10 @@ vim.opt.tabstop = 4
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Split options
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- Use system clipboard for copy/paste
 vim.opt.clipboard:append { 'unnamedplus' }
