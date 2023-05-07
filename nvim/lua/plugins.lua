b3c41967d2051579fcdbe76d4b68c84447dea92f
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-06 23:20:12 -0700

    nvim theme

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,9 +17,12 @@ return require('packer').startup(function(use)
     use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
-        requires = {
-            { 'nvim-lua/plenary.nvim' }
-        }
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
+
+    use {
+        'rose-pine/neovim',
+        as = 'rose-pine'
     }
 
     if packer_bootstrap then
