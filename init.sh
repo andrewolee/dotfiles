aed13b3796f4ecd5a61e3a0475fb71985605781a
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-07 22:04:05 -0700

    brewinstall

--- init.sh
+++ init.sh
@@ -7,9 +7,11 @@ function symlink {
         exit 1
     fi
 
-    if [ -e $2 ]
+    DEST=$2/$(basename $1)
+
+    if [ -e $DEST ]
     then
-        echo "A config file at $2 already exists! Overwrite with $1? [y/n]"
+        echo "Overwrite existing config at $DEST? [y/n]"
         read -q
         echo
 
@@ -18,17 +20,34 @@ function symlink {
             return
         fi
 
-        echo "Moving existing config at $2 to trash..."
-        echo $2/$(basename $1)
+        echo "Moving existing config at $DEST to trash..."
+        mv $DEST $HOME/.Trash
+    fi
+
+    echo "Creating symlink..."
+    ln -s $PWD/$1 $DEST
+}
+
+function brewinstall {
+    echo "Checking if $1 is installed..."
+    if brew list $1 &> /dev/null
+    then
+        echo "$1 is already installed."
+    else
+        echo "$1 not found. Installing..."
+        brew install $1
     fi
 }
+
+if ! which brew &> /dev/null
+then
+    echo "Install Homebrew!"
+    exit 1
+fi
 
 symlink nvim $HOME/.config
 symlink zsh/.zshrc $HOME
 
-brew install neovim
-brew install ripgrep
-brew install jandedobbeleer/oh-my-posh/oh-my-posh
-
-ln -s $PWD/nvim $HOME/.config
-ln -s $PWD/zsh/.zshrc $HOME
+brewinstall neovim
+brewinstall ripgrep
+brewinstall jandedobbeleer/oh-my-posh/oh-my-posh
