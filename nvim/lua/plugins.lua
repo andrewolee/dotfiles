747872fe8c5d608de3e2e3b892bde260214cc486
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-22 22:55:43 -0700

    autopairs

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,6 +30,16 @@ return require('packer').startup(function(use)
         run = ':TSUpdate'
     }
 
+    use {
+        'williamboman/mason.nvim',
+        run = ':MasonUpdate'
+    }
+
+    use {
+        'windwp/nvim-autopairs',
+        config = function() require('nvim-autopairs').setup {} end
+    }
+
     if packer_bootstrap then
         require('packer').sync()
     end
