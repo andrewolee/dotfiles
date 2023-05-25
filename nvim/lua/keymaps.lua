428989671333f1bbe9ec2ef73bac8ed34748f9fa
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-05-25 09:34:45 -0700

    autocomplete

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,3 +10,11 @@ vim.keymap.set('n', 'j', 'gj')
 
 -- Netrw
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+
+-- Split pane
+vim.keymap.set('n', 'ss', '<C-w>v')
+
+vim.keymap.set('n', 'sh', '<C-w>h')
+vim.keymap.set('n', 'sj', '<C-w>j')
+vim.keymap.set('n', 'sk', '<C-w>k')
+vim.keymap.set('n', 'sl', '<C-w>l')
