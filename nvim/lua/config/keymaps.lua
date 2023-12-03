bbad325209c9fb5104dd1c5e9e6ebd241789e2ff
Andrew Lee <132124185+andrewolee@users.noreply.github.com>
2023-12-02 18:11:23 -0800

    options

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Don't yank with x
+vim.keymap.set("n", "x", '"_x')
+
+-- Word wrap
+vim.keymap.set("n", "k", "gk")
+vim.keymap.set("n", "j", "gj")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Editor options
+vim.opt.wrap = true
+vim.opt.scrolloff = 5
+
+-- Tab options
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
